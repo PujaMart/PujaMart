11fe786916533a8f6da72a207c01b4f0b24ca030
shreyansh <shreyansh3536@gmail.com>
2024-07-09 20:46:15 +0545

    Intializationgit

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+//window.addEventListener('resize',() => {
+  //  location.reload();
+//});
+
+
+function CloseTopOffer() {
+    document.querySelector('.coupons-nav').style.display = 'none';
+}
+
+function svmenu() {
+    document.querySelector('.cat-nav-links').style.display = 'flex';
+    document.querySelector('.cat-container').style.display = 'flex';
+}
+
+function ShowMenu() {
+    document.getElementById('menu-btn').style.display = 'none';
+    document.getElementById('menu-close-btn').style.display = 'flex';
+    svmenu();
+}
+
+function CloseMenu() {
+    document.getElementById('menu-close-btn').style.display = 'none';
+    document.getElementById('menu-btn').style.display = 'flex';
+    document.querySelector('.cat-nav-links').style.display = 'none';
+    document.querySelector('.cat-container').style.display = 'none';
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    //Cart Count Update
+    let cartCounter = 0;
+
+    document.querySelectorAll(".cart-add").forEach(button => {
+        button.addEventListener("click", function() {
+        cartCounter++;
+        document.getElementById("Cart-Counter").textContent = cartCounter;
+        });
+    });
+
+
+    //Product Search
+    const searchInput = document.getElementById("site-search");
+    const searchImg = document.getElementById("search-img");
+
+    searchImg.addEventListener("click", function(event) {
+        event.preventDefault(); 
+
+        // Manually trigger form submission
+        const query = searchInput.value;
+        const currentUrl = window.location.href.split('?')[0]; // Get the base URL without query parameters
+        const redirect = `${currentUrl}?search_query=${encodeURIComponent(query)}`;
+
+        // Redirect to the constructed URL
+        window.location.href = redirect;
+    });
+});
+
+
