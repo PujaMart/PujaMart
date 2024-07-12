8c6ae3fef56456464de24233b4583dde606dd641
shreyansh <shreyansh3536@gmail.com>
2024-07-12 12:22:46 +0545

    Body Ready

--- scripts/script.js
+++ scripts/script.js
@@ -56,3 +56,22 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
+//Sliding Product Containers
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
